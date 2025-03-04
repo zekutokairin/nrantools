96c63efe27662dba53ba0de95c9042f3529f5cc9
Zeku Tokairin <zeku@heckfire.club>
2025-03-03 21:20:23 -0500

    Need to set colors and stuff, but CSV line generation works

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nrantools
 Scripts for importing and maintaining your personal arcade in N:RAN
+
+From working install, steps to take:
+# Download latest yt-dlp and overwrite old youtube-dl file
+# Download latest retroarch core nightlies
+
+# (Optional) Remove assorted trash
+## in New Retro Arcade Neon\NewRetroArcade\Saved\Config\WindowsNoEditor directory, open GameUserSettings and remove trash
---
+++ verify.py
@@ -0,0 +1,73 @@
+import os
+import shutil
+from tempfile import TemporaryDirectory
+
+from wand import image
+
+# New Retro Arcade:Neon Content directory
+BASE_DIRECTORY = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\New Retro Arcade Neon\\NewRetroArcade\\Content"
+TMP_DIRECTORY = "C:jj"
+
+# Make sure it's the right directory
+assert os.path.exists(os.path.join(BASE_DIRECTORY,"ArcadeCartridges.xml"))
+assert os.path.exists(os.path.join(BASE_DIRECTORY,"ArcadeMachines.xml"))
+assert os.path.exists(os.path.join(BASE_DIRECTORY,"ArcadeMusic.xml"))
+assert os.path.exists(os.path.join(BASE_DIRECTORY,"ArcadePosters.xml"))
+
+NES_COLOR = "#525151"
+SNES_COLOR = "#E5E5E5"
+
+# TODO: Create if doesn't exist
+OUTPUT_DIRECTORY = "output"
+
+ROMTYPES = ['nes','smc','sfc','md','gb','gbc','gba']
+IMAGETYPES = ['png','gif','jpg']
+
+def checkCartDirectory(path):
+    cart_dict = {}
+    csv = ""
+    # Given a directory of ROMs/artwork:
+    # 1) Verify that every ROM has artwork
+    # 2) Return a CSV string for the NRAN Arcade Manager
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            name, ext = os.path.splitext(f)
+
+            # It is a ROM
+            if ext[1:].lower() in ROMTYPES:
+                # Search the directory for a cover file
+                coverart = None
+                for i in range(len(name),1, -1):
+                    # Get all files starting with the same name
+                    maybe_coverart = [x for x in files if os.path.splitext(x)[0].startswith(name[0:i])]
+
+                    # We didn't find anything, try again with a shorter version of
+                    # the filename
+                    if len(maybe_coverart) == 0:
+                        continue
+
+                    if len(maybe_coverart) == 2:
+                        # The list should contain the rom and the art, remove the rom
+                        maybe_coverart.remove(f)
+                        #Game,Core,GameName,Texture,Colour,Type,FixedLocation,Include (Yes/No),,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+                        # Mega Man 2 (USA).nes,bnes_libretro.dll,Mega Man 2 (NES),output.dds,#333333,Console,CartridgeTable1,Yes,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+                        if f.endswith(".nes"):
+                            l = [f, "bsnes_retrocore.dll", name, maybe_coverart[0],"#1F1F1F","","","Yes"] 
+                        line = ','.join(l)+",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+                        print(line)
+
+                        #print("%s:%s" % (f, maybe_coverart[0]))
+                        break
+
+    return csv
+
+def ytBacklog():
+    # get user playlist public/unlinked
+    # return list of urls to write into CSV
+    pass
+
+if __name__ == "__main__":
+    # TODO: Create --delete option that deletes Carts, Tapes, and Posters not
+    #       in the user's content directory
+    checkCartDirectory(os.path.join("..","NES"))
+
