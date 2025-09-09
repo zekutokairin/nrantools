2648cc52cc3739f68782526c3b5433add3c6d568
Zeku <zeku@heckfire.club>
2025-09-09 08:42:37 -0400

    Returning backlog as a list of tuples

--- tapes.py
+++ tapes.py
@@ -6,25 +6,28 @@ import itertools
 
 URL = "https://www.youtube.com/playlist?list=PLBLdlvve0cQVCbWHF2bqfSWVLrzFGg3w8"
 
-t = tempfile.NamedTemporaryFile(delete=False)
-#TMPFILE = "D://transfar//urls.txt"
-TMPFILE = t.name
-print(t.name)
-
-#with ) as fp:
-subprocess.run(['yt-dlp','--flat-playlist','-i','--print-to-file','title,url',TMPFILE,URL])
-
-"""
-with open(fp.name) as url_file:
-    for title,url in itertools.zip_longest(*[url_file]*2):
-        print("%s:%s" % (title,url))
-
-"""
-
-#TODO: Get playlist name/url
-
-def ytBacklog():
+def ytBacklog(playlist_url):
     # get user playlist public/unlinked
     # return list of urls to write into CSV
-    pass
+    t = tempfile.NamedTemporaryFile(delete=False)
+    #TMPFILE = "D://transfar//urls.txt"
+    TMPFILE = t.name
+    print(t.name)
 
+    subprocess.run(['yt-dlp','--flat-playlist','-i','--print-to-file','title,url',TMPFILE,playlist_url])
+
+    # Backlog will be a list of tuples we return
+    backlog = []
+
+    with open(TMPFILE) as urlfile:
+        for title in urlfile:
+            title = title.strip()
+            url = next(urlfile).strip()
+            backlog.append((title,url))
+
+    return backlog
+
+if __name__ == "__main__":
+    yt_videos = ytBacklog(URL)
+    import code
+    code.interact(local=locals())
