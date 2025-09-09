5625f38153ce8c12d457ab26ffc1570683cd22e1
Zeku <zeku@heckfire.club>
2025-09-09 19:27:14 -0400

    Working vhs tape parsing and radio file parsing.

--- tapes.py
+++ tapes.py
@@ -1,38 +1,62 @@
 #!/usr/bin/env python
 import subprocess
-import yt_dlp
+import json
 import tempfile
-import itertools
+import hashlib
+import os
 
 URL = "https://www.youtube.com/playlist?list=PLBLdlvve0cQVCbWHF2bqfSWVLrzFGg3w8"
+MEDIAFILE = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\New Retro Arcade Neon\\NewRetroArcade\\Content\\UserMedia.json"
+LAYOUTFILE = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\New Retro Arcade Neon\\NewRetroArcade\\Content\\Layouts\\DadoPack.layout"
 
-# TODO: Theory, build database of entries into json, THEN add them in the Layout?
+MUSICFILE = os.path.join("tests","radio.txt")
+
+# TODO: Theory, build database of entries into UserMedia.json, THEN add them in the LayoutFile?
 # Lists of radio stations:
 #   https://asiadreamradio.torontocast.stream/stations/newstream.html
 
-def ytBacklog(playlist_url):
-    # get user playlist public/unlinked
-    # return list of urls to write into CSV
+def parseMusicFile():
+    musictapes = []
+    with open(MUSICFILE, encoding='utf-8') as fp:
+        for line in fp:
+            name, url = line.strip().split(",")
+            import code
+            #code.interact(local=locals())
+            tape_id = "Tape"+ hashlib.md5(url.encode('utf-8')).hexdigest()
+            print("%s:%s:%s" % (tape_id,name,url))
+        
+    """
+    with open(MEDIAFILE) as fp:
+        music_json = json.load(fp)
+        # TODO: finish returning the dictionary here
+    """
+
+def parseVideoTapes(playlist_url):
+    # With the given Youtube playlist, return a dictionary with keys ID, Name, URL
     t = tempfile.NamedTemporaryFile(delete=False)
-    #TMPFILE = "D://transfar//urls.txt"
     TMPFILE = t.name
-    print(t.name)
+    subprocess.run(['yt-dlp','--flat-playlist','-i','--print-to-file','id,title,url',TMPFILE,playlist_url])
 
-    subprocess.run(['yt-dlp','--flat-playlist','-i','--print-to-file','title,url',TMPFILE,playlist_url])
-
-    # Backlog will be a list of tuples we return
-    backlog = []
+    # Backlog will be a list of dictwe return
+    vhstapes = []
 
     with open(TMPFILE) as urlfile:
-        for title in urlfile:
-            title = title.strip()
+        for vid_id in urlfile:
+            vid_id = "VHSTape"+vid_id.strip()
+            title = next(urlfile).strip()
             url = next(urlfile).strip()
-            backlog.append((title,url))
 
-    return backlog
+            vhstapes.append({"ID":vid_id,"Name":title,"URL":url})
+
+    return vhstapes
+
+def writeUserMedia(music_dict,video_dict):
+    with open(MEDIAFILE,'w'):
+        pass
 
 if __name__ == "__main__":
     # TODO: Radio Stations
-    yt_videos = ytBacklog(URL)
+    music_dict = parseMusicFile()
+    #vhs_dict = parseVideoTapes(URL)
     import code
-    code.interact(local=locals())
+    #code.interact(local=locals())
---
+++ tests/ROADMAP.md
@@ -0,0 +1,36 @@
+New Retro Arcade: Neon Test Suite
+
+- TODO: Can we programmatically tell Steam to Verify Local Files?
+    - This would create a "known bad" baseline to start modding 
+
+= Currently Working (Titania) =
+- Arcade
+- VHS (Youtube)
+- 
+- Gameboy
+
+Arcades:
+- Classic
+- CPS2
+- Neo-Geo
+
+Game Cartridges:
+- NES
+- SNES
+- Genesis
+- GBA
+- Game Gear
+
+VHS Tapes:
+- Local File
+- Remote File Share
+- Youtube URL
+
+Cassettes:
+- Local mp3
+- Local FLAC
+- Internet Radio
+
+GameBoy Cartridges:
+- Gameboy 
+
