4abf68e738ab4c65dbb25a0d5740091eca5c1112
Zekū Tōkairin <zeku@heckfire.club>
2025-10-19 18:30:12 -0400

    Unnecessary split of cart reader

--- cartridges.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-for root, dirs, files in os.walk(start_directory):
-    for 
-    print(f"Current directory: {root}")
-        print(f"Subdirectories: {dirs}")
-            print(f"Files: {files}")
