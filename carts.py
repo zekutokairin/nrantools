022dfcb130c78abb057df525572d2f48f44b5f2e
Zekū Tōkairin <zeku@heckfire.club>
2025-10-25 12:39:19 -0400

    Working changes to cart

--- carts.py
+++ carts.py
@@ -6,6 +6,7 @@ import shutil
 import re
 
 # FIXME get this from environment?
+DEBUG = os.getenv("NRAN_DEBUG")
 
 #NRAN_CONTENT_DIR = os.getenv("NRAN_CONTENT_DIR")
 NRAN_CONTENT_DIR = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\New Retro Arcade Neon\\NewRetroArcade\\Content"
@@ -17,7 +18,7 @@ NRAN_ARTDIR = os.path.join(NRAN_CONTENT_DIR, "Cartridges")
 #PACKDIR = "/Users/user/Sync/Streaming/Games/NRAN/Zekupack/ROMs/cartridges"
 PACKDIR = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\New Retro Arcade Neon\\NewRetroArcade"
 
-NES_CORE = "bnes_libretro" 
+NES_CORE = "bnes_libretro.dll" 
 SNES_CORE = "bsnes_performance_libretro.dll" 
 GEN_CORE = "picodrive_libretro.dll"
 GBA_CORE = "mgba_libretro.dll"
@@ -43,13 +44,21 @@ def findCartridgeLabel(romname):
 
 if __name__ == "__main__":
     # Copy all subdirectories in
+
+    csv = ""
+    #Game,Core,GameVolume,GameImage,ScreenType,ButtonLayout,AB,XY,SS,ArtFrontPanelTexture,ArtFrontPanelColour,ArtSidePanelTexture,ArtSidePanelColour,AttractScreen,FixedLocation,GameMusicVolume,Include (Yes/No),,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    #10yard.zip,mame2014_libretro.dll,1.5,10Yard Splash.dds,HD,LayoutTetris,#000000,#000000,#000000,10Yard Cabinet Normal Front.dds,,10Yard Cabinet Normal Side.dds,,10yard.mp4,,1,No,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
     for dirpath, dirnames, filenames in os.walk(PACKDIR):
         for filename in filenames:
             if os.path.splitext(filename)[1][1:] in ROMTYPES:
                 romfile = os.path.join(dirpath,filename)
                 findCartridgeLabel(filename)
 
-                # TODO actually copy it
-                #shutil.copyfile(romfile,
+                if not DEBUG:
+                    shutil.copyfile(romfile, NRAN_ROMDIR)
+                    shutil.copyfile(romfile, NRAN_ARTDIR)
+
+                # TODO: Copy art 
 
 #supermarioworld.smc,bsnes_performance_libretro.dll,Super Mario World,snes_mario.dds,#FFFFFF,Console,,Yes
